17de276e271c382d15b7afcbba77854fac346390
azufb <curiousazunas@gmail.com>
2023-04-24 23:17:05 +0900

    グラフ描画コンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RecordingForm from './components/Form/RecordingForm';
+import SampleGraph from './components/Graph/SampleGraph';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <RecordingForm />
+        <SampleGraph />
       </header>
     </div>
   );
