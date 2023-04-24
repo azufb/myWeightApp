d8b2050c3a53d19144e0a951f805953f13d0e59b
azufb <curiousazunas@gmail.com>
2023-04-24 21:12:38 +0900

    datepicker実装

--- src/components/Form/RecordingForm.tsx
+++ src/components/Form/RecordingForm.tsx
@@ -1,7 +1,11 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+
+// react-datepicker用CSS
+import 'react-datepicker/dist/react-datepicker.css';
 
 const RecordingForm = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, control } = useForm();
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -11,6 +15,13 @@ const RecordingForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name='date'
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <DatePicker selected={value} onChange={onChange} />
+        )}
+      />
       <label htmlFor='weight'>Weight: </label>
       <input id='weight' {...register('weight')} />
 
