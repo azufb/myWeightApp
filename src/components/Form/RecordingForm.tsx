fba1cec11be107e68343470b1a7a113df757371a
azufb <curiousazunas@gmail.com>
2023-04-26 01:37:02 +0900

    フォームの値から、レコード追加の関数に渡すオブジェクトを作る

--- src/components/Form/RecordingForm.tsx
+++ src/components/Form/RecordingForm.tsx
@@ -1,20 +1,47 @@
 import { useForm, Controller } from 'react-hook-form';
 import DatePicker, { registerLocale } from 'react-datepicker';
 import ja from 'date-fns/locale/ja';
+import { format } from 'date-fns';
+import putCommandFunc from '../../aws/putCommandFunc';
 
 // react-datepicker用CSS
 import 'react-datepicker/dist/react-datepicker.css';
 
+// DatePicker用にロケーションをjaにセット
 registerLocale('ja', ja);
 
 const RecordingForm = () => {
   const { register, handleSubmit, reset, control } = useForm();
 
   const onSubmit = (data: any) => {
-    console.log(data);
+    // 日付フォーマット
+    const date: Date = new Date(data.date);
+    const dataYear: number = date.getFullYear();
+    const dataMonth: number = date.getMonth();
+    const dataDate: number = date.getDate();
+    const formattedDate: string = format(
+      new Date(dataYear, dataMonth, dataDate),
+      'yyyy/MM/dd'
+    );
 
-    // dynamoDBへ登録
+    // 体重
+    const weightFloat: number = parseFloat(data.weight);
 
+    // BMI計算(小数第2位まで表示)
+    const bmi: number = Math.round((weightFloat / 2.365444) * 100) / 100;
+
+    // DynamoDBへ登録するために渡すオブジェクト
+    const formData = {
+      date: formattedDate,
+      weight: weightFloat,
+      bmi: bmi,
+    };
+
+    console.log(formData);
+
+    // DynamoDBへ登録する関数実行
+
+    // フォームリセット
     reset();
   };
 
