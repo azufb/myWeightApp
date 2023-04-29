21dc2ca8de5c4d8ad8d80d95eb8b08c522674f80
azufb <curiousazunas@gmail.com>
2023-04-29 20:01:48 +0900

    アロー関数に修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import RecordingForm from './components/Form/RecordingForm';
 import SampleGraph from './components/Graph/SampleGraph';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <header className='App-header'>
@@ -11,6 +11,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
