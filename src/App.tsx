1e6c7189504c849d5b2c038487653bebce95bb4d
azufb <curiousazunas@gmail.com>
2023-04-29 22:41:29 +0900

    react queryを使ってデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import './App.css';
 import RecordingForm from './components/Form/RecordingForm';
 import SampleGraph from './components/Graph/SampleGraph';
+import { useQuery } from '@tanstack/react-query';
+import scanItemsFunc from './aws/scanItemsFunc';
 
 const App = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['data'],
+    queryFn: scanItemsFunc,
+  });
+
+  console.log(data);
+
   return (
     <div className='App'>
       <header className='App-header'>
