6995708a8486b7ab8ea84d78ae38491aa17922e1
azufb <curiousazunas@gmail.com>
2023-04-30 03:26:41 +0900

    サンプルデータのグラフは削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import RecordingForm from './components/Form/RecordingForm';
-import SampleGraph from './components/Graph/SampleGraph';
 import { useQuery } from '@tanstack/react-query';
 import scanItemsFunc from './aws/scanItemsFunc';
 import Graph from './components/Graph/Graph';
@@ -17,7 +16,6 @@ const App = () => {
     <div className='App'>
       <header className='App-header'>
         <RecordingForm />
-        <SampleGraph />
         <Graph />
       </header>
     </div>
