a5ed52be1a114bf60675d34a40fb5532156bda07
azufb <curiousazunas@gmail.com>
2023-05-03 19:54:42 +0900

    useQueryでテーブルの情報取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 import RecordingForm from './components/Form/RecordingForm';
 import { useQuery } from '@tanstack/react-query';
-import scanItemsFunc from './aws/scanItemsFunc';
 import Graph from './components/Graph/Graph';
 
 const App = () => {
   const { isLoading, error, data } = useQuery({
     queryKey: ['data'],
-    queryFn: scanItemsFunc,
+    queryFn: () =>
+      fetch('http://localhost:9000/getItems', {
+        method: 'GET',
+      }).then((result) => {
+        return result.json();
+      }),
   });
 
   console.log(data);
