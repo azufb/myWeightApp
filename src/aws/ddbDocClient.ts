250cd1675a5f94f9eb6b4230fa51ea3239d55bd8
azufb <curiousazunas@gmail.com>
2023-05-04 20:18:02 +0900

    不要なファイル削除

--- src/aws/ddbDocClient.ts
+++
@@ -1,6 +0,0 @@
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { ddbClient } from './ddbClient';
-
-const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
-
-export { ddbDocClient };
