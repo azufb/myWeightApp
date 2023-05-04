92503e720e0b774799a847cae4110b5042da8af6
azufb <curiousazunas@gmail.com>
2023-05-04 19:04:20 +0900

    データ取得結果の型定義

---
+++ src/types/GetResultDataType.ts
@@ -0,0 +1,20 @@
+export type ResultMetadataType = {
+  httpStatusCode: number;
+  attempts: number;
+  requestId: string;
+  totalRetryDelay: number;
+};
+
+export type ResultItemsType = {
+  date: string;
+  bmi: number;
+  weight: number;
+  timestamp: number;
+};
+
+export type GetResultDataType = {
+  $metadata: ResultMetadataType;
+  Count: number;
+  Items: ResultItemsType[];
+  ScannedCount: number;
+};
