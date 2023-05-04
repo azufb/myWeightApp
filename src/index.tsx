afdb5c0d2ae9e1698a0fa08d1d07415d9e4b43bf
azufb <curiousazunas@gmail.com>
2023-05-04 20:22:26 +0900

    型指定漏れ修正

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // React Queryを使うための設定
-const queryClient = new QueryClient({
+const queryClient: QueryClient = new QueryClient({
   defaultOptions: {
     queries: {
       // React Queryはデフォルトでコンポーネントにフォーカスを当てるとフェッチが動くので、無効にする
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
   },
 });
 
-const root = ReactDOM.createRoot(
+const root: ReactDOM.Root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
