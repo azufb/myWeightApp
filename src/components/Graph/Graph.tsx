f8f5a3432215915a45af9da224277d7a6b231b1f
azufb <curiousazunas@gmail.com>
2023-05-06 20:37:27 +0900

    グラフ部分の表示修正

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -20,27 +20,33 @@ const Graph = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>グラフ</h2>
-      <div className={styles.graph}>
-        <LineChart width={900} height={300} data={queryData?.Items}>
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis
-            dataKey='date'
-            interval={0}
-            angle={-16}
-            dx={-20}
-            dy={8}
-            tick={{
-              fontSize: 10,
-            }}
-          />
-          <YAxis dataKey='weight' />
-          {/*<YAxis yAxisId={2} dataKey='bmi' orientation='right' />*/}
-          <Tooltip />
-          <Line type='monotone' strokeWidth={2} dataKey='weight' />
-          {/*<Line yAxisId={2} type='monotone' dataKey='bmi' />*/}
-        </LineChart>
-      </div>
-      <p className={styles.annotation}>※X軸スクロールできます。</p>
+      {queryData?.Count === 0 ? (
+        <p>データがありません。</p>
+      ) : (
+        <>
+          <div className={styles.graph}>
+            <LineChart width={900} height={300} data={queryData?.Items}>
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis
+                dataKey='date'
+                interval={0}
+                angle={-16}
+                dx={-20}
+                dy={8}
+                tick={{
+                  fontSize: 10,
+                }}
+              />
+              <YAxis dataKey='weight' domain={[0, 100]} />
+              {/*<YAxis yAxisId={2} dataKey='bmi' orientation='right' />*/}
+              <Tooltip />
+              <Line type='monotone' strokeWidth={2} dataKey='weight' />
+              {/*<Line yAxisId={2} type='monotone' dataKey='bmi' />*/}
+            </LineChart>
+          </div>
+          <p className={styles.annotation}>※X軸スクロールできます。</p>
+        </>
+      )}
     </div>
   );
 };
